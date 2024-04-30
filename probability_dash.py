ce5b9701db01e330ba9029d58a240d4e121e436b
“CiciDu” <sd80@rice.edu>
2024-04-30 21:11:05 +0800

    intializing project

--- probability_dash.py
+++ probability_dash.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     dp.calculate_probability()
     dp.plot_probability_in_plotly(show_plot=False)
     dp.make_dash_for_main_plots()
+    server = dp.app.server
     dp.app.run_server(port=8052)
